f13d9259280e828c45c0aeb1fa1799081807beaa
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2025-01-09 20:07:30 -0300

    add icon drawer

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -4,6 +4,7 @@ import { ImUser } from "react-icons/im";
 import { MdFavorite } from "react-icons/md";
 import { HiMiniShoppingCart } from "react-icons/hi2";
 import { CiSearch } from "react-icons/ci";
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import DrawerSeacrch from "../Drawer";
 export default function Header(){
 
@@ -11,6 +12,7 @@ export default function Header(){
 
     return(
         <div className="container-header">
+            <HiOutlineMenuAlt1 size={35}/>
             {showDrawer?(
                 <DrawerSeacrch  showDrawer={showDrawer} setShowDrawer={setShowDrawer}/>
 
@@ -23,8 +25,8 @@ export default function Header(){
             </div>
             <h2>Logo</h2>
             <div className="icons-header">
-            <ImUser className="icon" size={25} />
-            <MdFavorite className="icon" size={25} />
+            <ImUser className="icon iconUser" size={25} />
+            <MdFavorite className="icon iconFavorito" size={25} />
             <HiMiniShoppingCart  className="icon" size={25}/>
  
 
