bfbeae79fa5571694d6629e1e2afa59487921909
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2025-01-14 12:35:44 -0300

    add storage firebase

---
+++ front-end/src/Service/firebaseConexao.jsx
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import {getStorage} from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHLVZhdhjuaD_KlrPWJIvk4CHE34RQONQ",
+  authDomain: "snackfast-6ef93.firebaseapp.com",
+  projectId: "snackfast-6ef93",
+  storageBucket: "snackfast-6ef93.appspot.com",
+  messagingSenderId: "380579335925",
+  appId: "1:380579335925:web:3a5113ac94fc192eafffbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app)
+
+export {app,storage};
