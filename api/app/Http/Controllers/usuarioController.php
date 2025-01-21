1e80f8be44a7fe46ab04bd3455e33f79c6354ee9
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2025-01-20 21:08:30 -0300

    add login

--- api/app/Http/Controllers/usuarioController.php
+++ api/app/Http/Controllers/usuarioController.php
@@ -10,38 +10,39 @@ use Illuminate\Support\Facades\Mail;
 
 class usuarioController extends Controller
 {
-    public function criarUsuario(Request $request){
+    public function criarUsuario(Request $request)
+    {
 
         $validated = $request->validate([
-            'email_usuario'=> ['required','string','unique:tb_usuario,email_usuario'],
-            'cpf_usuario'=> ['required','string','max:14','min:14'],
-            'nome_usuario'=>['required','string','max:20'],
-            'numero_usuario'=>['required','string','max:15'],
+            'email_usuario' => ['required', 'string', 'unique:tb_usuario,email_usuario'],
+            'cpf_usuario' => ['required', 'string', 'max:14', 'min:14'],
+            'nome_usuario' => ['required', 'string', 'max:20'],
+            'numero_usuario' => ['required', 'string', 'max:15'],
             'senha_usuario' => ['required', 'string', 'min:6'],
         ], [
             'email_usuario.required' => 'O e-mail é obrigatório.',
             'email_usuario.unique' => 'Este e-mail já está cadastrado.',
-        
+
             'cpf_usuario.required' => 'O CPF é obrigatório.',
             'cpf_usuario.max' => 'O CPF deve ter no máximo 14 caracteres.',
             'cpf_usuario.min' => 'O CPF deve ter exatamente 14 caracteres.',
-        
+
             'nome_usuario.required' => 'O nome é obrigatório.',
             'nome_usuario.max' => 'O nome deve ter no máximo 20 caracteres.',
-        
+
             'numero_usuario.required' => 'O número de telefone é obrigatório.',
             'numero_usuario.max' => 'O número de telefone deve ter no máximo 15 caracteres.',
-        
+
             'senha_usuario.required' => 'A senha é obrigatória.',
             'senha_usuario.min' => 'A senha deve ter pelo menos 6 caracteres.',
         ]);
 
         $usuario = new Usuario;
         $usuario->cpf_usuario = $request->cpf_usuario;
-        $usuario->nome_usuario= $request->nome_usuario;
-        $usuario->email_usuario=$request->email_usuario;
-        $usuario->numero_usuario=$request->numero_usuario;
-        $usuario->senha_usuario=$request->senha_usuario;
+        $usuario->nome_usuario = $request->nome_usuario;
+        $usuario->email_usuario = $request->email_usuario;
+        $usuario->numero_usuario = $request->numero_usuario;
+        $usuario->senha_usuario = $request->senha_usuario;
 
         $usuario->save();
         return response()->json(['message' => 'Usuário criado com sucesso!'], 201);
@@ -55,5 +56,21 @@ class usuarioController extends Controller
 
         // Mail::to($usuario->email_usuario)->send(new EmailVerificationMail($token));  
 
+    }
+
+    public function login(Request $request)
+    {
+        $usuario = Usuario::where('cpf_usuario', $request->cpf_usuario)->first();
+
+        if ($usuario && $request->senha_usuario === $usuario->senha_usuario) {
+            return response()->json([
+                'message' => 'Login como usuário bem-sucedido.',
+                $usuario
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Credenciais inválidas.'
+        ], 401);
     }
 }
