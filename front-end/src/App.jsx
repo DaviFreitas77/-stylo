1d8b02d752d33dd4cba9f02adb998dafc695a55f
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2025-03-17 16:05:44 -0300

    adicionar usequery no component colecao

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,12 @@
-
-
 import { AppRoute } from './Rotas'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 function App() {
- 
+  const queryClient = new QueryClient();  
   return (
-    <div className="app">
-      <AppRoute/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <AppRoute />
+    </QueryClientProvider>
   )
 }
 
--- front-end/src/components/Colecao/index.jsx
+++ front-end/src/components/Colecao/index.jsx
@@ -3,49 +3,56 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Loading from "../Loading/LoadingAnimation";
+import { useQuery } from "@tanstack/react-query";
 import './style.css'
 
+const fetchCategorias = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/getCategorias", {
+        method: "GET",
+    });
+
+    if (!response.ok) {
+        throw new Error("Erro ao buscar categorias");
+    }
+
+    return response.json();
+};
+
+
 export default function Colecao() {
-    const [categoria, setCategoria] = useState([]);
+    const { data: categoria, isLoading, isError, error } = useQuery({
+        queryKey: ["categorias"],
+        queryFn: fetchCategorias,
+    });
 
-    useEffect(() => {
-        const fetchCategoria = async () => {
-            try {
-                const response = await fetch("http://127.0.0.1:8000/api/getCategorias", {
-                    method: "GET",
-                });
-                const data = await response.json();
-                setCategoria(data);
-            } catch (error) {
-                console.error("Erro ao buscar categorias:", error);
-            }
-        };
-        fetchCategoria();
-    }, []);
+    if (isLoading) {
+        return <Loading />;
+    }
 
-
+    if (isError) {
+        return <div>Error: {error.message}</div>;
+    }
 
 
     return (
         <div className="container-colecao">
-            {categoria.length > 0 ?(
-                  categoria.map((item, index) => (
+            {categoria.length > 0 ? (
+                categoria.map((item, index) => (
                     <Link
                         to={`/produtos-categoria/${item.id_categoria}`}
-                        key={index} className="btnColecao">
-                        <img className="img-colecao " src={item.imagem_categoria} alt={item.nome_categoria} />
-    
+                        key={index}
+                        className="btnColecao"
+                    >
+                        <img
+                            className="img-colecao"
+                            src={item.imagem_categoria}
+                            alt={item.nome_categoria}
+                        />
                     </Link>
-    
-    
-    
-    
                 ))
-            ):(
-                <Loading/>
+            ) : (
+                <p>Nenhuma categoria encontrada.</p>
             )}
-
-          
         </div>
 
 
