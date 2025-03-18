0ab543aa2342c8590d944e3168505b90cd6bb498
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2025-03-18 20:26:33 -0300

    arrumando layout

--- front-end/src/pages/ProdutosCategoria/index.jsx
+++ front-end/src/pages/ProdutosCategoria/index.jsx
@@ -39,7 +39,9 @@ export default function ProdutosCategoria() {
 
 
     const filtrarProduto = async () => {
-
+        console.log('tamanho',idTamanho)
+        console.log('suncategoria',id_subCategoria)
+        console.log('cor',idCor)
         try {
             const response = await fetch(`http://127.0.0.1:8000/api/filtro/${id_subCategoria}/${idCor}/${idTamanho}`, {
                 method: 'GET'
@@ -126,118 +128,98 @@ export default function ProdutosCategoria() {
 
 
     return (
-        <div className="container-categoria-produtos">
-
-            <section className="container-filters">
-                <div className="filter-subCategoria">
-                    <h2 className="subtitulo-filter">Categoria</h2>
-                    {subCategoria.length > 0 && subCategoria.map((item, index) => (
-                        <button
-                            onClick={() => pegarCategoria(item.id_subCategoria)}
-                            className={`btn-subCategoiria ${id_subCategoria === item.id_subCategoria ? 'activeBtn' : ''}`}
-                            key={index}>
-                            {item.nome_subCategoria}
-                        </button>
-                    ))}
-                </div>
-                <div className="filter-subCategoria">
-                    <h2 className="subtitulo-filter">Cores</h2>
-
-                    {cor.length > 0 && cor.map((item, index) => (
-                        <div
-                            key={index}
-                            onClick={() => pegarCor(item.id_cor)}
-                            className="form-check">
-                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                            {item.desc_cor}
-                        </div>
-                    ))}
-
-                </div>
-                <div className="filter-subCategoria">
-                    <h2 className="subtitulo-filter">Tamanho</h2>
-
-                    {tamanho.length > 0 && tamanho.map((item, index) => (
-                        <div
-                            onClick={() => pegarTamanho(item.id_tamanho)}
-                            key={index}
-                            className="form-check">
-                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                            {item.desc_tamanho}
-                        </div>
-                    ))}
-
-                </div>
-                <div className="container-btn-filter">
-                    <button
-                        onClick={filtrarProduto}
-                        className="btn-aplicar">
-                        Aplicar
-                    </button>
-
-
-                </div>
-
-            </section>
-
-            <div className="produtos-categoria">
-
-
-                {produtoFiltrado.length > 0 ? (
-                    produtoFiltrado.map((item, index) => (
-
-
-                        <Link
-                            to={`/produto/${item.id_produto}`}
-                            key={index} className="container-produto-categoria">
-                            <div className="div-imagem-produto-categoria">
-                                <img
-                                    className="imagem-produto-categoria"
-                                    src={item.imagem_produto} alt="" />
-                            </div>
-                            <p className="nome-produto-categoria">{item.nome_produto}</p>
-                            <div className="div-preco-categoria">
-                                <div className="prices-categoria">
-                                    <p className="last-price-categoria">{item.preco_antigo_produto}</p>
-                                    <p className="price-produto-categoria ">{item.preco_produto}</p>
-                                </div>
-
-                                <button className="add-carrinho-card-categoria">
-                                    <  TbShoppingCartUp className="icon-carrinh-categoria" />
+        <div>
+            <Header/>
+            <div className="container-categoria-produtos">
+                <section className="container-filters">
+                    <div className="filter-group">
+                        <h2 className="subtitulo-filter">Categoria</h2>
+                        <div className="filter-options">
+                            {subCategoria.length > 0 && subCategoria.map((item, index) => (
+                                <button
+                                    onClick={() => pegarCategoria(item.id_subCategoria)}
+                                    className={`btn-subCategoria ${id_subCategoria === item.id_subCategoria ? 'activeBtn' : ''}`}
+                                    key={index}>
+                                    {item.nome_subCategoria}
                                 </button>
-                            </div>
-
-                        </Link>
-                    ))
-                ) : (
-                    produtos.map((item, index) => (
-                        <Link
-                            to={`/produto/${item.id_produto}`}
-                            key={index} className="container-produto-categoria">
-                            <div className="div-imagem-produto-categoria">
-                                <img
-                                    className="imagem-produto-categoria"
-                                    src={item.imagem_produto} alt="" />
-                            </div>
-                            <p className="nome-produto-categoria">{item.nome_produto}</p>
-                            <div className="div-preco-categoria">
-                                <div className="prices-categoria">
-                                    <p className="last-price-categoria">{item.preco_antigo_produto}</p>
-                                    <p className="price-produto-categoria ">{item.preco_produto}</p>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="filter-group">
+                        <h2 className="subtitulo-filter">Cores</h2>
+                        <div className="filter-options">
+                            {cor.length > 0 && cor.map((item, index) => (
+                                <label key={index} className="filter-checkbox">
+                                    <input type="checkbox" onChange={() => pegarCor(item.id_cor)} />
+                                    <span className="checkmark"></span>
+                                    {item.desc_cor}
+                                </label>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="filter-group">
+                        <h2 className="subtitulo-filter">Tamanho</h2>
+                        <div className="filter-options">
+                            {tamanho.length > 0 && tamanho.map((item, index) => (
+                                <label key={index} className="filter-checkbox">
+                                    <input type="checkbox" onChange={() => pegarTamanho(item.id_tamanho)} />
+                                    <span className="checkmark"></span>
+                                    {item.desc_tamanho}
+                                </label>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="container-btn-filter">
+                        <button onClick={filtrarProduto} className="btn-aplicar">Aplicar Filtros</button>
+                    </div>
+                </section>
+                <div className="produtos-categoria">
+                    {produtoFiltrado.length > 0 ? (
+                        produtoFiltrado.map((item, index) => (
+                            <Link
+                                to={`/produto/${item.id_produto}`}
+                                key={index} className="container-produto-categoria">
+                                <div className="div-imagem-produto-categoria">
+                                    <img
+                                        className="imagem-produto-categoria"
+                                        src={item.imagem_produto} alt="" />
                                 </div>
-
-                                <button className="add-carrinho-card-categoria">
-                                    <  TbShoppingCartUp className="icon-carrinh-categoria" />
-                                </button>
-                            </div>
-
-                        </Link>
-                    ))
-                )}
-
-
-
-
+                                <p className="nome-produto-categoria">{item.nome_produto}</p>
+                                <div className="div-preco-categoria">
+                                    <div className="prices-categoria">
+                                        <p className="last-price-categoria">{item.preco_antigo_produto}</p>
+                                        <p className="price-produto-categoria ">{item.preco_produto}</p>
+                                    </div>
+                                    <button className="add-carrinho-card-categoria">
+                                        <  TbShoppingCartUp className="icon-carrinh-categoria" />
+                                    </button>
+                                </div>
+                            </Link>
+                        ))
+                    ) : (
+                        produtos.map((item, index) => (
+                            <Link
+                                to={`/produto/${item.id_produto}`}
+                                key={index} className="container-produto-categoria">
+                                <div className="div-imagem-produto-categoria">
+                                    <img
+                                        className="imagem-produto-categoria"
+                                        src={item.imagem_produto} alt="" />
+                                </div>
+                                <p className="nome-produto-categoria">{item.nome_produto}</p>
+                                <div className="div-preco-categoria">
+                                    <div className="prices-categoria">
+                                        <p className="last-price-categoria">{item.preco_antigo_produto}</p>
+                                        <p className="price-produto-categoria ">{item.preco_produto}</p>
+                                    </div>
+                                    <button className="add-carrinho-card-categoria">
+                                        <  TbShoppingCartUp className="icon-carrinh-categoria" />
+                                    </button>
+                                </div>
+                            </Link>
+                        ))
+                    )}
+                </div>
             </div>
         </div>
 
